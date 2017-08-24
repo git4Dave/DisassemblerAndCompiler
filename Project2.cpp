68153ea4f1e42e816df4f2650135ef7d98cb1faa
git4Dave <ddmachin@yahoo.com>
2017-08-24 16:01:13 -0400

    Update Project2.cpp

--- Project2.cpp
+++ Project2.cpp
@@ -1,8 +1,5 @@
 //============================================================================
-// Name        : MIPSsim.cpp
 // Author      : David Machin
-// Description : On my honor, I have neither given nor received
-// 				 unauthorized aid on this assignment
 //============================================================================
 
 #include <iostream>
